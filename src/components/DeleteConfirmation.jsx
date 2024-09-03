077ce63c861c4df17742946302401715e5e11cbe
Awais Madni <awais.madni17@gmail.com>
2024-09-04 01:05:33 +0500

    "Added state and interval to track remaining time in DeleteConfirmation component" (That was another example of useEffect being needed to avoid an infinite loop and the cleanup function being needed to avoid ongoing processes behind the scenes)

--- src/components/DeleteConfirmation.jsx
+++ src/components/DeleteConfirmation.jsx
@@ -1,11 +1,26 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+
+const Timer = 3000;
 
 export default function DeleteConfirmation({ onConfirm, onCancel }) {
+  const [remainingTime, setRemainingTime] = useState(Timer);
+
+  useEffect(() => {
+    const interval= setInterval(() => {
+      console.log('INTERVAL');
+      setRemainingTime((prevTime) => prevTime - 10);
+    }, 10);
+
+    return () => {
+      clearInterval(interval);
+    }
+  }, []);
+
   useEffect(() => {
     console.log("TIMER_SET");
-    const timer= setTimeout(() => {
+    const timer = setTimeout(() => {
       onConfirm();
-    }, 3000);
+    }, Timer);
 
     return () => {
       console.log("Clearing up the timer");
@@ -25,6 +40,7 @@ export default function DeleteConfirmation({ onConfirm, onCancel }) {
           Yes
         </button>
       </div>
+      <progress value={remainingTime} max={Timer} />
     </div>
   );
 }
